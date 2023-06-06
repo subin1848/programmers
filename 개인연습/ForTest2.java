d0ac81aeec922c89c5a81b63e36b4c024c147002
subin1848 <s2315@e-mirim.hs.kr>
2023-06-07 01:08:40 +0900

    for2

---
+++ 개인연습/ForTest2.java
@@ -0,0 +1,15 @@
+class ForTest2
+{
+	public static void main (String args[])
+	{
+		int num1 = 0;
+		int num2;
+
+ 		for (num1 = 1; num1 <= 10; num1++)
+		{
+			num2 = 11 - num1;
+			System.out.println(num1 + "\t" + num2);
+		}
+		
+	}
+}
