18ca720927be16168df7b251cfc530bc143afde6
subin1848 <s2315@e-mirim.hs.kr>
2023-08-21 21:30:16 +0900

    foravgrank

--- ForAvgRank.java
+++ ForAvgRank.java
@@ -10,20 +10,15 @@ class ForAvgRank
 		System.out.println("**********************");
 		System.out.println("*       반평균       *");
 		System.out.println("**********************");
-		System.out.println();
 		System.out.println(); 
 		System.out.print("입력받을 반의 개수 (2~4) ==> ");
 		count = sc.nextInt();		//반 개수
-		System.out.print("입력받을 학생 수 (2~4) ==> ");
-		student = sc.nextInt();		//학생 수
+
 		while(true)
 		{
 			if(count > 1 && count < 5)
-			{	
-				System.out.println();
-				System.out.println("[ 총 " + count + "개의 반이 존재합니다. ]");
-				System.out.println("[ 각 학습별 학생 수는 " + student + "명 입니다. ]");
-				break;
+			{
+				break;				
 			}
 			else
 			{
@@ -31,12 +26,29 @@ class ForAvgRank
 				System.out.println("[ 입력 가능한 반은 2~4반입니다. 다시 입력해주세요. ]");
 				System.out.print("입력받을 반의 개수 (2~4) ==> ");
 				count = sc.nextInt();
+			}
+		}
+		System.out.print("입력받을 학생 수 (2~4) ==> ");
+		student = sc.nextInt();
+		System.out.println();
+
+		while(true)
+		{
+			if(student > 1 && student < 5)
+			{
+				System.out.printf("[ 총 %d개의 반이 존재합니다. ]\n", count);
+				System.out.printf("[ 각 학급별 학생 수는 %d명 입니다. ]", student);	
+				System.out.println();
+				break;
+			}
+			else
+			{
+				System.out.println("[ 입력 가능한 학생 수는 2~4명입니다. 다시 입력해주세요. ]");
 				System.out.print("입력받을 학생 수 (2~4) ==> ");
 				student = sc.nextInt();
-				
+				System.out.println();
 			}
-		}	
-
+		}
 		System.out.println();
 		int classCount[] = new int[count*student];	//입력한 점수
 		int sum[] = new int[count];		//각 반의 평균 합계
@@ -59,16 +71,14 @@ class ForAvgRank
 
 			}
 
-			sum[i%count] += classCount[i];	//sum[출력된 반] += score[입력한 점수]	//
+			sum[i%count] += classCount[i];	//sum[출력된 반] += score[입력한 점수]	
 		}
-
 		double classAvg[] = new double[count];	//각 반 평균
 
 		for(int i = 0; i < classAvg.length; i++)
 		{
 			classAvg[i] = sum[i]/(double)(student);
 		}
-
 		double maxAvg = classAvg[0], minAvg = classAvg[0];
 		int maxCount = 1, minCount = 1;
 
@@ -86,13 +96,12 @@ class ForAvgRank
 			}
 		}
 		System.out.println();
-
 		int classRank[] = new int[count];	//반 랭킹
+		
 		for(int i = 0; i < classRank.length; i++)
 		{
 			classRank[i] = 1;
 		}
-
 		for(int i = 0; i < classRank.length; i++)
 		{
 			for(int j = 0; j < classRank.length; j++)
@@ -104,14 +113,11 @@ class ForAvgRank
 			}
 
 		}
-
-
 		for(int i = 0; i < classRank.length; i++)
 		{
 			System.out.printf("[%d반] 점수: %.2f - [%d등]\n", (i+1), classAvg[i], classRank[i]);
 		}
 		System.out.println();
-		System.out.printf("[ 제일 잘한 반은 %d반이며, %d반의 평균 점수는 %.2f점으로, 제일 점수가 낮은 %d의 반의 평균은 %.2f점보다 %.2f점 높습니다. ]", maxCount, maxCount, maxAvg, minCount, minAvg, (maxAvg - minAvg));
-		
+		System.out.printf("[ 제일 잘한 반은 %d반이며, %d반의 평균 점수는 %.2f점으로, 제일 점수가 낮은 %d반의 평균은 %.2f점보다 %.2f점 높습니다. ]", maxCount, maxCount, maxAvg, minCount, minAvg, (maxAvg - minAvg));
 	}
 }
